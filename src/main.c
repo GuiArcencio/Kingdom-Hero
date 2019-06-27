8db06a5a25343149745ecf26d02f9ac5a5f4e552
Guilherme Arcencio <guiarcencio@hotmail.com>
2019-06-27 00:11:01 -0300

    Criando arquivo base

---
+++ src/main.c
@@ -0,0 +1,55 @@
+#include <allegro5/allegro5.h>
+
+#define SCREEN_W 640
+#define SCREEN_H 480
+
+int main() {
+    al_init();
+    al_install_keyboard();
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
+    ALLEGRO_EVENT_QUEUE* fila_eventos = al_create_event_queue();
+    al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+    al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+    al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+    ALLEGRO_DISPLAY* janela = al_create_display(SCREEN_W, SCREEN_H);
+    al_set_window_title(janela, "Jogo daora");
+
+    al_register_event_source(fila_eventos, al_get_keyboard_event_source());
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+    al_register_event_source(fila_eventos, al_get_timer_event_source(timer));
+
+    bool sair = false;
+    bool desenha = false;
+
+    ALLEGRO_EVENT evento;
+
+    al_start_timer(timer);
+    while (!sair) {
+        al_wait_for_event(fila_eventos, &evento);
+
+        switch (evento.type) {
+            case ALLEGRO_EVENT_TIMER:
+                desenha = true;
+                break;
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                sair = true;
+                break;
+        }
+
+
+        // desenhar na tela
+        if (desenha && al_is_event_queue_empty(fila_eventos)) {
+            al_clear_to_color(al_map_rgb(0, 255, 100));
+
+            al_flip_display();
+            desenha = false;
+        }
+    }
+
+    al_destroy_display(janela);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(fila_eventos);
+
+    return 0;
+}
