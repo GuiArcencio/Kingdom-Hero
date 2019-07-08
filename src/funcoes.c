20be20d20933addc6c3cb68586d5974f5b53cb2c
Caio <caio.appolination@hotmail.com>
2019-07-08 13:41:13 -0300

    Atualizando funcoes

--- src/funcoes.c
+++ src/funcoes.c
@@ -1,14 +1,43 @@
-#include "header/funcoes.h"
+-#include "header/funcoes.h"
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-float aumentaVelocidade(unsigned int pont){ //pont == pontua��o
+float aumentaVelocidade(unsigned int pont){ //pont == pontuação
     return sqrt(pont/100.0) + 1;
 }
 
-void salvaRanking(Jogador j){ //Salva o nome e a pontua��o em um arquivo
-    FILE *p = fopen("Ranking.txt", "rb");
+void salvaRanking(Jogador j){ //Salva o nome e a pontuação em um arquivo
+    FILE *p = fopen("Ranking.bin", "rb+");
+    int numJogadores;  //Registra o número de jogadores no ranking
+    if (p == NULL){
+        FILE *fp = fopen("Ranking.bin", "wb+");
+        fwrite(1, sizeof(int), 1, fp);
+        fwrite(j.nome,sizeof(char),3,fp);
+        fwrite(j.pont,sizeof(int),1,fp);
+
+    }else{
+        int num = fread(numJogadores, sizeof(int), 1, p); // num recebe a quantidade de dados lida
+        if((numJogadores < 5) || (num == 0))
+            fwrite(numJogadores + 1, sizeof(int), 1, p);
+        int i;
+        Jogador jRanking[5]; //Jogadores que já estão no ranking
+        for(i = 0; i < numJogadores; i++){
+            fread(jRanking[i].nome,sizeof(char),3,p);
+            fread(jRanking[i].pont,sizeof(int), 1,p);
+            if(jRanking[i].pont < j.pont){
+                fseek(p, -1 * sizeof(int), SEEK_CUR);
+                fseek(p, -3 * sizeof(char), SEEK_CUR);
+                fwrite(j.nome,sizeof(char),3,p);
+                fwrite(j.pont,sizeof(int),1,p);
+                /*Atribuindo às variáveis o nome e a pontuação do jogador substituído no
+                ranking para efetuar nova verificação com os outros jogadores no ranking*/
+                strcpy(j.nome,jRanking[i].nome);
+                j.pont = jRanking[i].pont;
+            }
+        }
+    }
 }
 /*void main(){
     int n;
